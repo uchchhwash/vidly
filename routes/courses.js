0c525ac29dc6a705b8370f7dd481a40673cc060f
uchchhwash <uchchhwash.git@gmail.com>
2020-06-24 03:34:18 +0600

    courses put, delete and get course by id

--- routes/courses.js
+++ routes/courses.js
@@ -23,6 +23,32 @@ router.post('/', (req, res) => {
     res.send(course);
 });
 
+router.put('/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+router.delete('/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+router.get('/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+    res.send(course);
+});
 
 function validateCourse(course) {
     const schema = {
