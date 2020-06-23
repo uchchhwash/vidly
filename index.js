1400d4d2ec2b075f68fb247639231aeb07ebcbf9
uchchhwash <uchchhwash.git@gmail.com>
2020-06-24 03:25:18 +0600

    custom middleware logger add

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
+const logger = require('./middleware/logger');
 const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
+app.use(logger);
+app.use('/', home);
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    debug('Morgan enabled...');
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
