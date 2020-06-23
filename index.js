51b5b66df2daf34195d63e3dd1ad1b15afdc4b0d
uchchhwash <uchchhwash.git@gmail.com>
2020-06-24 03:28:39 +0600

    pug view engine and config files add

--- index.js
+++ index.js
@@ -8,13 +8,21 @@ const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views'); // default
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
 app.use(logger);
+
 app.use('/', home);
 
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
