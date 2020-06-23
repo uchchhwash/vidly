dbcfcff9d2bebc2e31996724d0913fda989a1eb2
uchchhwash <uchchhwash.git@gmail.com>
2020-06-24 03:31:17 +0600

    api genra put

--- routes/genres.js
+++ routes/genres.js
@@ -23,6 +23,17 @@ router.post('/', (req, res) => {
     res.send(genre);
 });
 
+router.put('/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
 
 
 
