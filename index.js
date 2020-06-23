1523c8a316cb66a94b35c28c374e3858ee8649f0
uchchhwash <uchchhwash.git@gmail.com>
2020-06-24 03:26:57 +0600

    all middleware add

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
 app.use(logger);
 app.use('/', home);
+
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
