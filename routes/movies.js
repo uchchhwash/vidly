30639423d651eb03c8b7c025482dc179e36b6cd6
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 03:35:19 +0600

    get movies implement

--- routes/movies.js
+++ routes/movies.js
@@ -3,3 +3,8 @@ const { Genre } = require("../models/genre");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+
+router.get("/", async(req, res) => {
+    const movies = await Movie.find().sort("name");
+    res.send(movies);
+})
