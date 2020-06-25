f364969c5b91685b73d77a98f320819e3fd52671
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 02:02:49 +0600

    get request by id implement

--- routes/customers.js
+++ routes/customers.js
@@ -89,6 +89,13 @@ router.patch('/:id', async(req, res) => {
     res.send(customer);
 });
 
+router.get('/:id', async(req, res) => {
+    const customer = await Customer.findById(req.params.id);
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+    res.send(customer);
+});
 
 function validateCustomer(customer) {
     const schema = {
