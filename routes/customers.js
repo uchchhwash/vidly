e1d33014a0d975b5ddec48623b03d1fdc2780ffe
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 01:01:56 +0600

    post customer implement

--- routes/customers.js
+++ routes/customers.js
@@ -44,7 +44,20 @@ router.get('/', async(req, res) => {
     res.send(customers);
 });
 
+router.post('/', async(req, res) => {
+    const { error } = validateCustomer(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    let customer = new Customer({
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone,
+        email: req.body.email
+    })
+
+    customer = await customer.save();
+    res.send(customer);
+});
 
 function validateCustomer(customer) {
     const schema = {
