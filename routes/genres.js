e03e0a6462f28b522e159087a323d99605324528
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-25 21:45:50 +0600

    post request for genre fix

--- routes/genres.js
+++ routes/genres.js
@@ -22,11 +22,11 @@ router.post('/', (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const genre = {
-        id: genres.length + 1,
+    let genre = new Genre({
         name: req.body.name
-    };
-    genres.push(genre);
+    });
+
+    genre = await genre.save();
     res.send(genre);
 });
 
