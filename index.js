50a91e8d02eddddafa9a806a0b1e434acd8b47da
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-25 23:10:16 +0600

    customer schema & valdiation

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
-mongoose.connect("mongodb://localhost/vidly")
+mongoose.connect("mongodb://localhost/vidly", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB..."))
     .catch((err) => console.error("Could Not Connect to MongoDB"))
 
--- routes/customers.js
+++ routes/customers.js
@@ -1,4 +1,40 @@
+const { isEmail, isMobilePhone } = require("validator")
 const Joi = require('joi');
 const mongoose = require("mongoose") //.set("debug", true);
 const express = require('express');
 const router = express.Router();
+
+const customerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    isGold: Boolean || false,
+    phone: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return isMobilePhone(value, "bn-BD")
+            },
+            message: "Invalid Mobile Number"
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return isEmail(value);
+            },
+            message: "Invalid Email"
+        }
+    }
+})
+
+const Customer = new mongoose.model("Customer", customerSchema);
+
+
+module.exports = router;
