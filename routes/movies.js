56ba99d418b436318d1fb175a615df72ad297996
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 03:39:45 +0600

    post movies implement

--- routes/movies.js
+++ routes/movies.js
@@ -8,3 +8,24 @@ router.get("/", async(req, res) => {
     const movies = await Movie.find().sort("name");
     res.send(movies);
 })
+
+.post("/", async(req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send("Invalid genre.");
+
+    let movie = new Movie({
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    });
+    movie = await movie.save();
+
+    res.send(movie);
+});
