0311bc07b1217a4e5b46076e398ee076ad923471
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 05:05:26 +0600

    put request for movies implment

--- routes/movies.js
+++ routes/movies.js
@@ -30,4 +30,28 @@ router.get("/", async(req, res) => {
     res.send(movie);
 });
 
+router.put('/:id', async(req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre.');
+
+    const movie = await Movie.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    }, { new: true });
+
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+
+    res.send(movie);
+});
+
+
+
 module.exports = router;
