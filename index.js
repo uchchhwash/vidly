11f383d94285b407f57ba33f675f70c0a43df95a
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 06:01:20 +0600

    rentals route init

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
 const logger = require("./middleware/logger");
+const rentals = require("./routes/rentals");
 const movies = require("./routes/movies");
 const customers = require("./routes/customers");
 const genres = require("./routes/genres");
@@ -24,6 +25,7 @@ app.use(express.static("public"));
 app.use(helmet());
 app.use(logger);
 
+app.use("/api/rentals", rentals);
 app.use("/api/movies", movies);
 app.use("/api/customers", customers);
 app.use("/api/genres", genres);
---
+++ routes/rentals.js
@@ -0,0 +1,8 @@
+const { Rental, validate } = require("../models/rental");
+const { Movie } = require("../models/movie");
+const { Customer } = require("../models/customer");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
