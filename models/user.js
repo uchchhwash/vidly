6263b3a1b188f78798b2437d695708011097a2f5
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 21:56:04 +0600

    user create post request implement

--- models/user.js
+++ models/user.js
@@ -30,9 +30,9 @@ const User = new mongoose.model("User", new mongoose.Schema({
 
 function validateUser(user) {
     const schema = {
-        name: Joi.string().min(3).required(),
+        name: Joi.string().min(5).required(),
         email: Joi.string().email().required(),
-        password: Joi.string.min(8).max(50).required()
+        password: Joi.string().min(8).max(50).required()
     };
 
     return Joi.validate(user, schema);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const { User, validate } = require("../models/user")
 const express = require("express");
-const { route } = require("./genres");
 const router = express.Router();
 
 router.get("/", async(req, res) => {
@@ -8,5 +7,21 @@ router.get("/", async(req, res) => {
     res.send(user);
 })
 
+.post("/", async(req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    console.log(req.body)
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send("User already registered");
+
+    user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    await user.save();
+    res.send(user);
+})
 
 module.exports = router;
