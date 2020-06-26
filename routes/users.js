ea9e8d1c537e333f46b964ffa0ad892a16ddbfb8
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 21:19:44 +0600

    get all user implement

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const { User, validate } = require("../models/user")
 const express = require("express");
+const { route } = require("./genres");
 const router = express.Router();
+
+router.get("/", async(req, res) => {
+    const user = await User.find().sort("name");
+    res.send(user);
+})
 
 
 module.exports = router;
