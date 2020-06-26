0756f8f11b9ba87d6793e6a009cf767245e4ec7a
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-26 06:11:36 +0600

    post request for rental implement

--- routes/rentals.js
+++ routes/rentals.js
@@ -10,4 +10,37 @@ router.get("/", (req, res) => {
     res.send(rentals);
 })
 
+router.post('/', async(req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findById(req.body.customerId);
+    if (!customer) return res.status(400).send('Invalid customer.');
+
+    const movie = await Movie.findById(req.body.movieId);
+    if (!movie) return res.status(400).send('Invalid movie.');
+
+    if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+
+    let rental = new Rental({
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            phone: customer.phone,
+            email: customer.email
+        },
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate
+        }
+    });
+    rental = await rental.save();
+
+    movie.numberInStock--;
+    movie.save();
+
+    res.send(rental);
+});
+
 module.exports = router;
