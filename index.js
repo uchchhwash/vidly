3363e8ca54a147dfc145e78351d870facc69f8d9
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-27 09:46:38 +0600

    unhandleRejections

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ const app = express();
 
 process.on("uncaughtException", (err) => {
     logger.error(err.message, { metadata: err });
+    process.exit(1);
+})
+
+process.on("unhandledRejection", (err) => {
+    logger.error(err.message, { metadata: err });
+    process.exit(1);
 })
 
 if (!config.get("jwtPrivateKey")) {
