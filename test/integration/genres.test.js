943aa777f9986aee4d982709f5c7def674d45c08
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-28 02:24:55 +0600

    fix getAllGenre test

--- test/integration/genres.test.js
+++ test/integration/genres.test.js
@@ -1,17 +1,22 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genre")
 let server;
 
 describe("/api/genres", () => {
     beforeEach(() => {
         server = require("../../index");
     })
-    afterEach(() => {
+    afterEach(async() => {
         server.close();
+        await Genre.remove({});
     })
     describe("GET /", () => {
         it("should return all genres", async() => {
+            await Genre.collection.insertMany([{ name: "genre1" }, { name: "genre2" }]);
             const res = await request(server).get("/api/genres");
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
         });
+
     })
 })
