24ae3baff8a3abe767b860b6bded50f70934dafd
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-28 02:28:35 +0600

    another expect implement to  getAllGenre test

--- test/integration/genres.test.js
+++ test/integration/genres.test.js
@@ -16,7 +16,8 @@ describe("/api/genres", () => {
             const res = await request(server).get("/api/genres");
             expect(res.status).toBe(200);
             expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === "gene1")).toBeTruthy();
+            expect(res.body.some(g => g.name === "genre2")).toBeTruthy();
         });
-
     })
 })
