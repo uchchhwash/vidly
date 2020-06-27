8172c6eaa39bc18021bfe2e19ad017b1408bcd28
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-27 06:00:02 +0600

    logger implement successful

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,6 @@
 const logger = require("./logger")
 
 module.exports = function(err, req, res, next) {
-
-    console.log(err.message)
-        // logger.error(err.message, err);
+    logger.error(err.message, err);
     res.status(500).send("Something Failed");
 }
