1bdb8a580518df404c134f9b5112e60c65386256
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-28 02:09:49 +0600

    get genres for integration implement

--- startup/logging.js
+++ startup/logging.js
@@ -2,13 +2,12 @@ const logger = require("../middleware/logger");
 require("express-async-errors");
 module.exports = function() {
     process.on("uncaughtException", (err) => {
-        console.log("uncaught call");
         logger.error(err.message, { metadata: err });
-        // process.exit(1);
+        //process.exit(1);
     })
 
     process.on("unhandledRejection", (err) => {
         logger.error(err.message, { metadata: err });
-        // process.exit(1);
+        //process.exit(1);
     })
 }
