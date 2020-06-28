636f7bdfd7b573e3c76bb7b5e58d7d8201a3bd79
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-28 06:57:23 +0600

    logger fix

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,32 +2,33 @@ const config = require("config");
 const winston = require("winston");
 const { createLogger, format } = require('winston');
 const { combine, timestamp, metadata, printf, json, splat } = format;
-require("winston-mongodb");
+(process.env.NODE_ENV !== "test") ? require("winston-mongodb"): null;
 
 const logFormat = printf(({ level, message, timestamp }) => {
     return `Timestamp: ${timestamp}  Level: [${level}]  Message: ${message}`;
 });
 
-const logger = createLogger({
-    format: combine(
-        timestamp(),
-        logFormat
-    ),
-    transports: [
-        new winston.transports.Console(),
-        new winston.transports.MongoDB({ db: config.get("db"), metaKey: "metadata", options: { useUnifiedTopology: true } }),
-        // new transports.File({ filename: "logfile.log", level: "error" }),
-    ],
-});
+if (process.env.NODE_ENV !== "test") {
+    module.exports = createLogger({
+        format: combine(
+            timestamp(),
+            logFormat
+        ),
+        transports: [
+            new winston.transports.Console(),
+            new winston.transports.MongoDB({ db: config.get("db"), metaKey: "metadata", options: { useUnifiedTopology: true } }),
+            // new transports.File({ filename: "logfile.log", level: "error" }),
+        ],
+    });
 
-const loggerForTest = createLogger({
-    format: combine(
-        timestamp(),
-        logFormat
-    ),
-    transports: [
-        new winston.transports.Console(),
-    ],
-});
-
-module.exports = (process.env.NODE_ENV === "test") ? loggerForTest : logger;
+} else {
+    module.exports = createLogger({
+        format: combine(
+            timestamp(),
+            logFormat
+        ),
+        transports: [
+            new winston.transports.Console(),
+        ],
+    });
+}
--- test/integration/auth.test.js
+++ test/integration/auth.test.js
@@ -3,25 +3,17 @@ const { User } = require("../../models/user");
 let server;
 
 describe("auth middleware", () => {
-    console.log(process.env.NODE_ENV)
     beforeEach(() => {
         server = require("../../index");
-        token = new User().generateAuthToken();
     })
     afterEach(async() => {
         server.close();
     })
-    let token;
-    const exec = () => {
-        return request(server)
-            .post('/api/genres')
-            .set('x-auth-token', token)
-            .send({ name: "genre1" })
-    }
-
     it("should return 401 if no token is provided", async() => {
-        token = "";
-        const res = await exec();
+        const token = new User().generateAuthToken();
+        const res = await request(server)
+            .post('/api/genres')
+            .send({ name: "genre1" })
         expect(res.status).toBe(401);
     })
 })
