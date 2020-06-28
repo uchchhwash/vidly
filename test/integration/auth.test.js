7253777779eed7c42ced0b9249effda2703e753b
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-28 07:15:50 +0600

    auth integration tests

--- test/integration/auth.test.js
+++ test/integration/auth.test.js
@@ -1,19 +1,41 @@
 const request = require("supertest");
 const { User } = require("../../models/user");
+const { Genre } = require("../../models/genre");
 let server;
 
 describe("auth middleware", () => {
+    let token;
+
     beforeEach(() => {
         server = require("../../index");
+        token = new User().generateAuthToken();
     })
     afterEach(async() => {
+        await Genre.remove({});
         server.close();
     })
-    it("should return 401 if no token is provided", async() => {
-        const token = new User().generateAuthToken();
-        const res = await request(server)
+
+    const exec = () => {
+        return request(server)
             .post('/api/genres')
+            .set('x-auth-token', token)
             .send({ name: "genre1" })
+    }
+
+    it("should return 401 if no token is provided", async() => {
+        token = "";
+        const res = await exec();
         expect(res.status).toBe(401);
-    })
+    });
+
+    it("should return 400 if token is invalid", async() => {
+        token = "aaaa";
+        const res = await exec();
+        expect(res.status).toBe(400);
+    });
+
+    it("should return 200 if token is valid", async() => {
+        const res = await exec();
+        expect(res.status).toBe(200);
+    });
 })
