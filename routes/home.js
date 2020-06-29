5209bd30712d021d4cb520759cbd26e71c8a7438
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-06-30 05:18:29 +0600

    minor changes

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const config = require("config");
 
 router.get("/", (req, res) => {
+    res.render("index", { title: config.get("name"), message: "Welcome To Vidly Rental Application" });
+});
+
+router.get("/api", (req, res) => {
     res.status(200).send({
             name: config.get("name"),
             version: config.get("version"),
