1f89e1974f8b332f2dc5fa852240576874507e08
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-03 20:09:10 +0600

    support init

--- startup/routes.js
+++ startup/routes.js
@@ -8,6 +8,7 @@ const movies = require("../routes/movies");
 const customers = require("../routes/customers");
 const genres = require("../routes/genres");
 const logs = require("../routes/logs");
+const support = require("../routes/support");
 const home = require("../routes/home");
 const error = require("../middleware/error");
 
@@ -22,6 +23,7 @@ module.exports = function(app) {
     app.use("/api/customers", customers);
     app.use("/api/genres", genres);
     app.use("/api/logs", logs);
+    app.use("/api/support", support);
     app.use("/", home);
     app.use(error);
 }
