ea4ac6fef70bb50cea3ab826806a1b5f009abbe4
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-03 19:34:54 +0600

    new method on support model

--- models/support.js
+++ models/support.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
-const { isEmail } = require("validator")
+const { isEmail } = require("validator");
+const { Customer } = require("./customer");
+const { User } = require("./user");
+
 const supportSchema = new mongoose.Schema({
+    user: {
+        _id: mongoose.Types.ObjectId,
+        userType: String
+    },
     title: {
         type: String,
         minlength: 5,
-        required
+        required: true
     },
     message: {
         type: String,
         minlength: 5,
-        required
+        required: true
     },
     email: {
         type: String,
@@ -20,3 +27,27 @@ const supportSchema = new mongoose.Schema({
         },
     }
 })
+
+supportSchema.methods.isUser = function(user) {
+    if (!user) {
+        this.user._id = new mongoose.Types.ObjectId,
+            this.user.userType = "Anonymous"
+    } else {
+        this.user._id = user;
+        let check = Customer.findById(user);
+        if (check) {
+            this.user.userType = "Customer";
+        } else {
+            let check = User.findById(user);
+            if (check) {
+                this.user.userType = "Customer";
+            }
+        }
+    }
+}
+const Support = new mongoose.model("Support", supportSchema);
+
+const test = new Support()
+console.log(test)
+
+exports.Support = Support;
